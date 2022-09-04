948925b536e62f8c25f87f626a8cbdea19eea2f8
Jean 'henyxia' Wasilewski <henyxia@revs0.com>
2022-09-04 16:07:14 +0200

    feat(challenge): add 0.0.3 knn solver

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ from flask import request
 import hashlib
 import hmac
 
+from sklearn.neighbors import KNeighborsClassifier
+
 app = Flask(__name__)
 
 @app.route("/version")
 def version():
-    return jsonify(version="0.0.2")
+    return jsonify(version="0.0.10")
 
 @app.post('/identify')
 def identidy_post():
@@ -28,3 +30,21 @@ def identidy_post():
     ).hexdigest()
 
     return jsonify(digest=digest)
+
+@app.post('/algo/knn/class')
+def algo_knn_class():
+    req = request.get_json()
+
+    neigh = KNeighborsClassifier(n_neighbors=5)
+    #pprint.pprint(X)
+    neigh.fit(
+        req["X"],
+        req["Y"],
+    )
+    correct_class = neigh.predict(req["test_value"])
+    precisions = neigh.predict_proba(req["test_value"])
+
+    return jsonify(
+        predicted_class=int(correct_class[0]),
+        precision=float(precisions[0][correct_class[0]]),
+    )
